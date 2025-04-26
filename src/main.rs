a158fb31b9f6406847772386eb486ebb3db0c7e2
Zlendy <zlendy@proton.me>
2025-04-26 12:54:11 +0200

    feat: Create basic HTTP REST API with Axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::get};
+use serde_json::json;
+use tokio::net::TcpListener;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(OpenApi)]
+#[openapi(paths(hello))]
+pub struct ApiDoc;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/hello", get(hello))
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[utoipa::path(
+    get,
+    path = "/hello",
+    responses(
+        (status = 200, description = "Hello, world!")
+    )
+)]
+async fn hello() -> impl IntoResponse {
+    (StatusCode::OK, Json(json!({ "message": "Hello, world!" })))
 }
