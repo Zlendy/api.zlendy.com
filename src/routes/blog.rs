Zlendy/api.zlendy.com

commit cdf742acf2c4fc27745bf1627b04ac6ae7467a24

refactor: Use a short-lived sync Mutex instead of a long-lived async RwLock

src/routes/blog.rs
@@ -1,5 +1,9 @@
 use core::option::Option::Some;
-use std::{collections::HashMap, error::Error};
+use std::{
+    collections::HashMap,
+    error::Error,
+    sync::{Arc, Mutex},
+};
 
 use axum::{
     Json, debug_handler,
@@ -11,15 +15,17 @@ use chrono::{DateTime, Utc};
 use serde::Serialize;
 use utoipa::ToSchema;
 
-use crate::SharedAppState;
+use crate::AppState;
 
-pub type BlogRoutes = HashMap<String, BlogValue>;
+pub type SharedBlogState = Arc<Mutex<BlogState>>;
 
 #[derive(Default, Debug, Clone)]
 pub struct BlogState {
     value: Option<BlogRoutes>,
     last_modified: DateTime<Utc>,
 }
+
+pub type BlogRoutes = HashMap<String, BlogValue>;
 
 #[derive(Default, Debug, Clone)]
 pub struct BlogValue {
@@ -88,16 +94,16 @@ async fn update_routes(
 #[debug_handler]
 pub async fn get_metadata(
     Path(slug): Path<String>,
-    State(state): State<SharedAppState>,
+    State(state): State<AppState>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let SharedAppState { args, blog } = state;
-    let mut blog = blog.write().await;
+    let AppState { args, blog } = state;
+    let mut blog_state = blog.lock().expect("mutex was poisoned").clone();
 
-    let routes = match blog.value.clone() {
-        Some(routes) if expired_cache(blog.last_modified, 5) => {
+    let routes = match blog_state.value.clone() {
+        Some(routes) if expired_cache(blog_state.last_modified, 5) => {
             // Routes cache has expired
             let routes = update_routes(Some(routes), args.zlendy_url.clone()).await;
-            blog.last_modified = Utc::now();
+            blog_state.last_modified = Utc::now();
 
             routes
         }
@@ -105,7 +111,7 @@ pub async fn get_metadata(
         None => {
             // Routes cache does not exist
             let routes = update_routes(None, args.zlendy_url.clone()).await;
-            blog.last_modified = Utc::now();
+            blog_state.last_modified = Utc::now();
 
             routes
         }
@@ -115,7 +121,14 @@ pub async fn get_metadata(
         return Err(StatusCode::SERVICE_UNAVAILABLE);
     };
 
-    blog.value = Some(routes.clone());
+    blog_state.value = Some(routes.clone());
+
+    {
+        // Mutex guard is unlocked outside this scope
+        let mut blog_mutex = blog.lock().expect("mutex was poisoned");
+        blog_mutex.value = blog_state.value.clone();
+        blog_mutex.last_modified = blog_state.last_modified;
+    }
 
     let Some(value) = routes.get(&slug) else {
         return Err(StatusCode::NOT_FOUND);
@@ -123,7 +136,7 @@ pub async fn get_metadata(
 
     // TODO: Load data from Umami Analytics and store it in BlogMetadata
 
-    println!("{}, {:#?}, {:#?}", slug, args, blog);
+    println!("{}, {:#?}, {:#?}", slug, args, blog_state);
 
     Ok(Json(value.metadata.clone()))
 }

src/main.rs
@@ -1,12 +1,12 @@
-use std::{borrow::Cow, error::Error, sync::Arc, time::Duration};
+use std::{borrow::Cow, error::Error, time::Duration};
 
 use args::Args;
 use axum::{
     Router, error_handling::HandleErrorLayer, http::StatusCode, response::IntoResponse,
     routing::get,
 };
-use routes::blog::BlogState;
-use tokio::{net::TcpListener, sync::RwLock};
+use routes::blog::SharedBlogState;
+use tokio::net::TcpListener;
 use tower::{BoxError, ServiceBuilder};
 use tower_http::compression::CompressionLayer;
 use utoipa::OpenApi;
@@ -17,8 +17,8 @@ pub mod routes;
 use crate::routes::blog;
 
 #[derive(Default, Clone)]
-pub struct SharedAppState {
-    blog: Arc<RwLock<BlogState>>,
+pub struct AppState {
+    blog: SharedBlogState,
     args: Args,
 }
 
@@ -31,7 +31,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::load()?;
     let address = format!("{}:{}", args.host, args.port);
 
-    let mut state = SharedAppState::default();
+    let mut state = AppState::default();
     state.args = args;
 
     let app = Router::new()

commit c79a947feada9a51db09cd6c0bac8dfe7325f4b5

fix: Mutex is not preventing duplicate updates
Bug was introduced in cdf742acf2c4fc27745bf1627b04ac6ae7467a24 (oops)

src/routes/blog.rs
@@ -1,8 +1,5 @@
 use core::option::Option::Some;
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
+use std::{collections::HashMap, sync::Arc};
 
 use axum::{
     Json, debug_handler,
@@ -12,6 +9,7 @@ use axum::{
 };
 use chrono::{DateTime, Utc};
 use serde::Serialize;
+use tokio::sync::Mutex;
 use utoipa::ToSchema;
 
 use crate::AppState;
@@ -49,6 +47,8 @@ async fn update_routes(
     current_routes: Option<BlogRoutes>,
     host: String,
 ) -> Result<BlogRoutes, reqwest::Error> {
+    println!("update_routes");
+
     let response = reqwest::get(format!("{host}/blog.json"))
         .await?
         .json::<HashMap<String, Option<String>>>()
@@ -96,7 +96,7 @@ pub async fn get_metadata(
     State(state): State<AppState>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let AppState { args, blog } = state;
-    let mut blog_state = blog.lock().expect("mutex was poisoned").clone();
+    let mut blog_state = blog.lock().await;
 
     let routes = match blog_state.value.clone() {
         Some(routes) if expired_cache(blog_state.last_modified, 5) => {
@@ -121,13 +121,6 @@ pub async fn get_metadata(
     };
 
     blog_state.value = Some(routes.clone());
-
-    {
-        // Mutex guard is unlocked outside this scope
-        let mut blog_mutex = blog.lock().expect("mutex was poisoned");
-        blog_mutex.value = blog_state.value.clone();
-        blog_mutex.last_modified = blog_state.last_modified;
-    }
 
     let Some(value) = routes.get(&slug) else {
         return Err(StatusCode::NOT_FOUND);